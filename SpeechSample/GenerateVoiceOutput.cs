b654400ba1161e29354e3f71639b38599f809942
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-07-20 11:07:36 +0800

    Add project files.

---
+++ SpeechSample/GenerateVoiceOutput.cs
@@ -0,0 +1,19 @@
+﻿using SpeechSample;
+//using WeihanLi.Extensions;
+//using WeihanLi.Extensions;
+
+public class GenerateVoiceOutput
+{
+    public static async Task MainTestAsync()
+    {
+        const string txtFilePath = @"C:\Users\xiyueyu\Desktop\questions.txt";
+        var index = 0;
+        foreach (var line in await File.ReadAllLinesAsync(txtFilePath))
+        {
+            if (string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line)) continue;
+            index++;
+            var outputFileName = $"{index}-output.wav";
+            await Helper.TextToSpeechAsync(line, outputFileName, "zh-CN-XiaoshuangNeural");
+        }
+    }
+}
